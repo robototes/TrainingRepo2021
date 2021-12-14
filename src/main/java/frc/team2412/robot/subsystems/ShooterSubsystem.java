0548ec585800ef62373295a42b82a836929143b1
jamietheraccoon <gabe_guitron@yahoo.com>
2021-12-13 21:42:22 -0800

    Add PID controllers to ShooterSubsystem

--- src/main/java/frc/team2412/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/ShooterSubsystem.java
@@ -1,6 +1,9 @@
 package frc.team2412.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANPIDController;
+import com.revrobotics.ControlType;
+import com.robototes.PIDControls.PIDController;
 import com.robototes.units.Distance;
 import com.robototes.units.UnitTypes;
 import edu.wpi.first.wpilibj.CAN;
@@ -26,6 +29,11 @@ public class ShooterSubsystem extends SubsystemBase implements Loggable{
         public static double MAX_ROTATION_SPEED = 5.0;
         @Config.NumberSlider
         public static double MIN_ROTATION_SPEED = 0.5;
+
+        public static final double P = 0.0004;
+        public static final double I = 0.000001;
+        public static final double D = 0.0001;
+        public static final double FF = 0;
 	}
 
 	// Enums will go below here
@@ -36,9 +44,9 @@ public class ShooterSubsystem extends SubsystemBase implements Loggable{
 	// Public so other subsystems can access it, but final so they cant mess with it (@Log allows us to log the object)
 	// motors for shooting the balls
     @Log
-	public final CANSparkMax motor1;
+	public final CANSparkMax leftMotor;
     @Log
-    public final CANSparkMax motor2;
+    public final CANSparkMax rightMotor;
     // motor for rotating the flywheel
     @Log
     public final CANSparkMax motor3;
@@ -48,14 +56,22 @@ public class ShooterSubsystem extends SubsystemBase implements Loggable{
     @Log
     public final Servo servo2;
 
+    public final CANPIDController leftPIDController;
+    public final CANPIDController rightPIDController;
+
 	// Private so other subsystems dont mess with it but this one can
 	private ShooterSubsystemState shooterSubsystemState;
 
 	// Create the example subsystem
-	public ShooterSubsystem(CANSparkMax motor1, CANSparkMax motor2, CANSparkMax motor3, Servo servo1, Servo servo2) {
-		this.motor1 = motor1;
-        this.motor2 = motor2;
-        this.motor2.setInverted(true);
+	public ShooterSubsystem(CANSparkMax leftMotor, CANSparkMax rightMotor, CANSparkMax motor3, Servo servo1, Servo servo2) {
+        this.leftMotor = leftMotor;
+        this.leftPIDController = this.leftMotor.getPIDController();
+
+        this.rightMotor = rightMotor;
+        this.rightMotor.setInverted(true);
+        this.rightPIDController = this.rightMotor.getPIDController();
+
+        setPIDtoDefault();
 
         this.motor3 = motor3;
 
@@ -69,16 +85,21 @@ public class ShooterSubsystem extends SubsystemBase implements Loggable{
         // clamp speed between MIN_SHOOT_SPEED and MAX_SHOOT_SPEED
         double correctedSpeed = Math.max(Math.min(speed, ShooterConstants.MAX_SHOOT_SPEED), ShooterConstants.MIN_SHOOT_SPEED);
 
-        this.motor1.set(correctedSpeed);
-        this.motor2.set(correctedSpeed);
+        this.leftMotor.set(correctedSpeed);
+        this.rightMotor.set(correctedSpeed);
         this.shooterSubsystemState = ShooterSubsystemState.ON;
     }
 
     public void shooterOff() {
         // stop both motors
-        this.motor1.set(0);
-        this.motor2.set(0);
+        this.leftMotor.set(0);
+        this.rightMotor.set(0);
         this.shooterSubsystemState = ShooterSubsystemState.OFF;
+    }
+
+    public void setRPM(double wantedSpeed) {
+        leftPIDController.setReference(wantedSpeed, ControlType.kVelocity);
+        rightPIDController.setReference(wantedSpeed, ControlType.kVelocity);
     }
 
     public void setHood(double degrees) {
@@ -113,6 +134,31 @@ public class ShooterSubsystem extends SubsystemBase implements Loggable{
 		return shooterSubsystemState == ShooterSubsystemState.ON;
 	}
 
+    public void setPIDtoDefault() {
+        setP(ShooterConstants.P);
+        setI(ShooterConstants.I);
+        setD(ShooterConstants.D);
+        setFF(ShooterConstants.FF);
+    }
 
+    public void setP(double p) {
+        this.leftPIDController.setP(p);
+        this.rightPIDController.setP(p);
+    }
+
+    public void setI(double i) {
+        this.leftPIDController.setI(i);
+        this.rightPIDController.setI(i);
+    }
+
+    public void setD(double d) {
+        this.leftPIDController.setD(d);
+        this.rightPIDController.setD(d);
+    }
+
+    public void setFF(double ff) {
+        this.leftPIDController.setFF(ff);
+        this.rightPIDController.setFF(ff);
+    }
 
 }
